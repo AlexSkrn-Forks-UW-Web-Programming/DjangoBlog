c5e5bf3a46e11a698ffc9085dcc6c60403028b57
alexskrn <askrn123@gmail.com>
2018-07-30 21:11:54 +0300

    Added Category model, a test case for it, registered the Category model in admin.py, added a function to views.py and made related changes to myblog/urls.py and mysite/urls.py

--- mysite/myblog/models.py
+++ mysite/myblog/models.py
@@ -1,4 +1,4 @@
-"""This module defines Python classes for our blog system."""
+"""This module defines Python classes/Django models for our blog system."""
 
 from django.db import models
 from django.contrib.auth.models import User
@@ -9,6 +9,7 @@ class Post(models.Model):
 
     title = models.CharField(max_length=128)
     text = models.TextField(blank=True)
+    # A single author can have many posts, while each post has only one author.
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
@@ -16,3 +17,18 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Category(models.Model):
+    """Define a class to represent categories our blog posts may fall into."""
+
+    name = models.CharField(max_length=128)
+    description = models.TextField(blank=True)
+    # Any given Post might belong in more than one Category
+    posts = models.ManyToManyField(Post, blank=True, related_name='categories')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-# from django.urls import path
 from django.conf.urls import url
+from django.urls import path, include
 
 # urlpatterns = [
 #     path('admin/', admin.site.urls),
 # ]
 
-urlpatterns = [url(r'^admin/', admin.site.urls), ]
+urlpatterns = [url(r'^admin/', admin.site.urls),
+               path('', include('myblog.urls')),
+               ]
