8ce7bc84d36b8f4565ef6e6a3d4e50c17fab2243
alexskrn <askrn123@gmail.com>
2018-07-31 23:15:39 +0300

    Followed lesson 7 assignment to make posts categorizable only from posts but not from categories pages

--- mysite/myblog/admin.py
+++ mysite/myblog/admin.py
@@ -2,5 +2,30 @@ from django.contrib import admin
 from myblog.models import Post
 from myblog.models import Category
 
-admin.site.register(Post)
-admin.site.register(Category)
+
+class MembershipInline(admin.TabularInline):
+
+    model = Category.posts.through
+
+
+class PostAdmin(admin.ModelAdmin):
+
+    # Can only add categories to posts
+    inlines = [
+               MembershipInline,
+               ]
+
+
+class CategoryAdmin(admin.ModelAdmin):
+
+    # If the following is uncommented,
+    # you can also categorize posts from
+    # the category page
+    # inlines = [
+    #            MembershipInline,
+    #           ]
+    exclude = ('posts', )
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
